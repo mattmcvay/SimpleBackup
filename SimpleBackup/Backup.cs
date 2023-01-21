642f43508fb86480b6d98094724a980fbf09b490
mattmcvay <matt.mcvay@gmx.com>
2023-01-21 14:32:55 -0500

    Add project files.

---
+++ SimpleBackup/Backup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Timers;
+using System.IO;
+
+namespace SimpleBackup
+{
+    public class Backup
+    {
+        private readonly System.Timers.Timer _timer;
+
+        public Backup()
+        {
+            _timer = new System.Timers.Timer(20000) {AutoReset = true};  // 5 seconds
+
+            _timer.Elapsed += timerElapsed;
+        }
+
+        private void timerElapsed(object sender, ElapsedEventArgs e)
+        {
+            // Do the work
+            string sourceDir = @"C:\Users\Matt\Desktop\NeedsBackedUp\";
+            string destination = @"C:\Users\Matt\Desktop\Backup\";
+            //string destination = @"F:\SimpleBackupTesting\";  // works
+
+            string[] extensions = new string[] { "*.gif", "*.xls", "*.xlsx", "*.txt", "*.pdf", "*.docx", "*.csv" };
+
+            foreach (string extension in extensions)
+            {
+                string[] allFiles = Directory.GetFiles(sourceDir, extension, SearchOption.AllDirectories);
+
+                foreach (string textFile in allFiles)
+                {
+                    string fileName = textFile.Substring(sourceDir.Length);
+                    string directoryPath = Path.Combine(destination, Path.GetDirectoryName(fileName));
+                    if (!Directory.Exists(directoryPath))
+                        Directory.CreateDirectory(directoryPath);
+
+                    File.Copy(textFile, Path.Combine(directoryPath, Path.GetFileName(textFile)), true);
+                }
+            }
+            Console.ReadLine();
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop() 
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ SimpleBackup/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace SimpleBackup
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitcode = HostFactory.Run(x => 
+            {
+                x.Service<Backup>(s =>
+                {
+                    s.ConstructUsing(backup => new Backup());
+                    s.WhenStarted(backup => backup.Start());
+                    s.WhenStopped(backup => backup.Stop());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("SimpleBackupService");
+                x.SetDisplayName("Simple Backup");
+                x.SetDescription("Backs up selected files.");
+
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitcode, exitcode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
