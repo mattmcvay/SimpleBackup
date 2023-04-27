64bf53c494918056cd8b59bce822bf4474e5d191
matt.mcvay <matt.mcvay@gmx.com>
2023-04-26 22:12:49 -0400

    4.26.2023 Update

--- SimpleBackup/Backup.cs
+++ SimpleBackup/Backup.cs
@@ -19,6 +19,8 @@ namespace SimpleBackup
         {
              _timer = new System.Timers.Timer(60000 * 720) {AutoReset = true};  // Every 12 hours.
 
+            //_timer = new System.Timers.Timer(10000) { AutoReset = false };  -- Left in for testing
+
             _timer.Elapsed += timerElapsed;
         }
 
@@ -27,24 +29,24 @@ namespace SimpleBackup
             Tuple<string, string> con = GetConnection.getConn();
             string sourceDir = con.Item1;
             string destination = con.Item2;
+           
+            string[] extensions = new string[] { ".gif", ".xls", ".xlsx", ".txt", ".pdf", ".doc", ".docx", ".csv", ".ppt", ".pptx" };
 
-            string[] extensions = new string[] { "*.gif", "*.xls", "*.xlsx", "*.txt", "*.pdf", "*.docx", "*.csv" };
+            string[] allFiles = Directory.GetFiles(sourceDir, "*.*", SearchOption.AllDirectories);
 
-            foreach (string extension in extensions)
+            foreach (string textFile in allFiles)
             {
-                string[] allFiles = Directory.GetFiles(sourceDir, extension, SearchOption.AllDirectories);
-
-                foreach (string textFile in allFiles)
+                if (extensions.Any(textFile.Contains))
                 {
-                    string fileName = textFile.Substring(sourceDir.Length);
-                    string directoryPath = Path.Combine(destination, Path.GetDirectoryName(fileName));
-                    if (!Directory.Exists(directoryPath))
-                        Directory.CreateDirectory(directoryPath);
+                string fileName = textFile.Substring(sourceDir.Length);
+                string directoryPath = Path.Combine(destination, Path.GetDirectoryName(fileName));
+                if (!Directory.Exists(directoryPath))
+                    Directory.CreateDirectory(directoryPath);
 
-                    File.Copy(textFile, Path.Combine(directoryPath, Path.GetFileName(textFile)), true);
-                }
-            }
-            //Console.ReadLine();
+                File.Copy(textFile, Path.Combine(directoryPath, Path.GetFileName(textFile)), true);
+                GetConnection.InsertIntoBackupLog(textFile, directoryPath);
+                }                              
+            }         
         }
 
         public void Start()
@@ -75,6 +77,23 @@ namespace SimpleBackup
             string destination = dt.Rows[0]["Destination"].ToString();
             con.Close();
             return Tuple.Create(source, destination);
+        }
+
+        public static void InsertIntoBackupLog(string fileName, string backupLocation)
+        {
+            var ConString = ConfigurationManager.ConnectionStrings["BackupConn"].ConnectionString;
+            SqlConnection con = new SqlConnection(ConString);
+            var dateNow = DateTime.Now;
+
+            con.Open();
+            SqlCommand cmd = new SqlCommand("dbo.insertRecord", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@FileName", fileName);
+            cmd.Parameters.AddWithValue("@BackupLocation", backupLocation);
+
+            cmd.ExecuteNonQuery();
+            con.Close();
         }
     }
 }
